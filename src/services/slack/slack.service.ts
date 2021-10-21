0212b369648bcda7127d8fcf9e7b437175dd1db3
Joseph Dillon <joseph.dillon.522@gmail.com>
2021-10-20 20:38:03 -0400

    slack integration works

--- src/services/slack/slack.service.ts
+++ src/services/slack/slack.service.ts
@@ -1,19 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { App } from '@slack/bolt';
 import { DatabaseService } from '../database/database.service';
+import { Member } from '@slack/web-api/dist/response/UsersListResponse';
 
 
 @Injectable()
 export class SlackService {
+    public app = new App({
+        token: this.config.get<string>('SLACK_BOT_TOKEN'),
+        signingSecret: this.config.get<string>('SLACK_SIGNING_SECRET'),
+        socketMode: true,
+        appToken: this.config.get<string>('SLACK_PERSTAT_BOT_SOCKET_TOKEN')
+    });
+
 
     constructor(
+        private config: ConfigService,
         private db: DatabaseService
-    ) { }
+    ) {
+        this.connectToSlack();
+    }
 
+    private async connectToSlack() {
+        await this.app.start(this.config.get<number>('SLACK_SOCKET_PORT'));
+    }
 
     public async getBotUsers() {
-        const res = await this.db.client.query('SELECT * FROM public."BOT_USER_INFO";');
+        // const res = await this.db.client.query('SELECT * FROM public."BOT_USER_INFO";');
 
-        return res.rows;
+        const usersList = await this.app.client.users.list();
+        let users: Member[] = usersList.members?.filter(user => !user.is_bot && !user.deleted && user.name != 'slackbot') || [];
+
+        // During local development if we only want to ping a single user
+        if (process.env.SEND_ONLY_TO_USER) {
+            users = users.filter(user => user.id === process.env.SEND_ONLY_TO_USER);
+        }
+        return users;
     }
 }
